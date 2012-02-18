a740b621bc3aa609bb51b7706a75922ac5fa7357
Ramakrishnan <raamkrish23@gmail.com>
2012-02-18 22:24:17 +0530

    nitial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BitsProject::Application.config.secret_token = '93ac585746700a800a3481006e900cc4f30d5c219e70bd772c5dbe67d6f1977dea882a5f72e67bf0771e13fce05620b2e6a4a11df0831a27219c8568fa5d2db6'
