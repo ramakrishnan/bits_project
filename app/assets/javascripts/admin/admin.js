63a8166cad524a092c6e06ce3c47a8b6f8286ba3
Ramakrishnan <raamkrish23@gmail.com>
2012-02-27 00:00:02 +0530

    Form a page by adding templates

--- app/assets/javascripts/admin/admin.js
+++ app/assets/javascripts/admin/admin.js
@@ -1,6 +1,37 @@
 var Admin = function () {
+	
+	var page = function() {
+
+	var initilize = function () {
+		$("#available_placeholders div").click(selectPlaceholder);
+		$("#add_placeholder").click(addPlaceholder);
+		$("#save_page").click(collectPlaceholders);
+	};
+	
+	var collectPlaceholders = function () {
+		if (activeRowWidth() < 966) {
+			alert("You have an incomplete row");
+			return false;
+		}
+		var plaxeHolder = $("#placeholders");
+		
+		plaxeHolder.empty();
+		var rows = $("#page_layout div.row");
+		rows.each(function (rIndex, row) {
+			rIndex++;
+			$(row).find("div").each(function (cIndex, column) {
+				$('<input>').attr({
+				    type: 'hidden',
+				    name: 'row['+rIndex+'][]',
+				    value: $(column).data('template_id'),
+				}).appendTo(plaxeHolder);				
+			});
+		});
+		return confirm("Are you sure");	
+	};
+	
 	var selectPlaceholder = function (event) {
-			$("#available_placeholders").find(".selected").removeClass("selected");
+		$("#available_placeholders").find(".selected").removeClass("selected");
 			$(event.target).toggleClass("selected");
 	};
 
@@ -8,15 +39,15 @@ var Admin = function () {
 		var layoutContiner = $("#page_layout");	
 		var selected = $("#available_placeholders").find(".selected:first");
 		
-		if (activeRow().length == 0 || ((966 - activeRowFree()) == 0 ) )  {
+		if (activeRow().length == 0 || ((966 - activeRowWidth()) == 0 ) )  {
 			layoutContiner.append("<div class='row'></div>");
 			activeRow().append(selected.clone().removeClass("selected"));
 		}
-		else if ((966 - activeRowFree()) >= selected.data("width"))  {
+		else if ((966 - activeRowWidth()) >= selected.data("width"))  {
 			activeRow().append(selected.clone().removeClass("selected"));
 		}
 		else {
-			console.log("Select proper");
+			alert("Select proper");
 		}
 	};
 
@@ -25,7 +56,7 @@ var Admin = function () {
 		return $("#page_layout").find("div.row:last");
 	};
 	
-	var activeRowFree = function() {
+	var activeRowWidth = function() {
 		var curWidth = 0;
 		activeRow().children().each(function(index,element) {
 		    curWidth += $(element).data("width");
@@ -34,8 +65,13 @@ var Admin = function () {
 		return curWidth;
 	};
 	
+	return {
+		initilize: initilize
+		}	
+	}();
+
+	
 return {
-	selectPlaceholder: selectPlaceholder,
-	addPlaceholder: addPlaceholder
+	page: page
 };
 }();
--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,7 +1,26 @@
 class Admin::PagesController < ApplicationController
 layout 'admin'
 	def create
-		
+		rows = params[:row]
+		if rows.length == 0
+			flash[:notice] = "Invalid templates for page"	
+			render :action => "new" and return
+		end		
+		@page = Page.new(params[:page])
+		if @page.save
+			puts rows.class
+			puts "//////////////"
+			rows.each do |row, columns|
+				puts columns.inspect
+				puts columns.class
+				columns.each_with_index do |template, index|
+					index = index + 1
+					@page.placeholders.create(:row => row, :column => index,
+						:template_id => template)
+				end
+			end
+		end
+		redirect_to :action => new_admin_page_path
 	end
 	
 	def new
