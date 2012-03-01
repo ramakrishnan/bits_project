0f7fd22fb0e64276cb1045390ee7873a5f8adc80
Ramakrishnan <raamkrish23@gmail.com>
2012-03-01 23:51:07 +0530

    Validate widget and placeholder

--- app/controllers/admin/pages/widgets_controller.rb
+++ app/controllers/admin/pages/widgets_controller.rb
@@ -5,4 +5,15 @@ class Admin::Pages::WidgetsController < ApplicationController
 		@page = Page.includes(:placeholders => [:grid, :holdings,
 					 :widgets]).find(params[:page_id])	 
 	end
+	
+	def create
+		placeholder = Placeholder.find(params[:placeholder])
+		res = placeholder.holdings.create(:widget_id => params[:widget])
+		if res.errors.blank?
+			flash[:notice] = "Widget added"
+		else
+			flash[:errors] = res.errors.full_messages.join("<br/>")
+		end	
+		redirect_to  admin_page_widgets_path(params[:page_id])
+	end
 end
--- app/models/holding.rb
+++ app/models/holding.rb
@@ -1,4 +1,26 @@
 class Holding < ActiveRecord::Base
 	belongs_to :widget
 	belongs_to :placeholder
+	validates_uniqueness_of :widget_id, :scope => :placeholder_id,
+		:message => "already taken"
+	validates_presence_of :widget_id, :placeholder_id
+	before_save :validate_widget_width
+	after_create :set_position
+	
+	def validate_widget_width
+		if self.widget.width != self.placeholder.grid.width
+			errors.add(:widget, "do not fit under the selected placeholder")
+		end
+	end
+	
+	def set_position
+		self.position = self.id
+		self.save
+	end
+	
+	def move_up
+	end
+	
+	def move_down
+	end
 end
