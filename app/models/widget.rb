a7a5d8540de5a22efd611d6b1cacf06439bdab12
Ramakrishnan <raamkrish23@gmail.com>
2012-03-04 03:48:59 +0530

    Load raw html ad scripts from database directly

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -3,6 +3,10 @@ class Widget < ActiveRecord::Base
 	has_many :collections
 	has_many :placeholders, :through => :holdings
 	
-	validates_presence_of :filename, :if => Proc.new{|w| w.widget_type != WidgetType::TYPES["Advertisement"] }  
+	validates_presence_of :filename, :unless => Proc.new{|w| w.is_advertisement?}  
 	validates_presence_of :name, :width, :widget_type
+	
+	def is_advertisement?
+		widget_type == WidgetType::TYPES["Advertisement"]
+	end
 end
