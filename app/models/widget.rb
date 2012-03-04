b497bd8ca0380ccb252d6f269edc38a0010fbf9e
Ramakrishnan <raamkrish23@gmail.com>
2012-03-05 00:11:20 +0530

    Look up value for widget file names

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -6,6 +6,14 @@ class Widget < ActiveRecord::Base
 	validates_presence_of :filename, :unless => Proc.new{|w| w.is_advertisement?}  
 	validates_presence_of :name, :width, :widget_type
 	
+		
+	FILETYPES = [["header", "/widgets/header.html.erb"],
+		["footer", "/widgets/footer.html.erb"],
+		["search", "/widgets/search.html.erb"],
+		["video_player", "/widgets/video_player.html.erb"],
+		["4 item slider", "/widgets/video_slider_485.html.erb"],
+		["home page static content", "/widgets/homepage_content.html.erb"]]
+
 	def is_advertisement?
 		widget_type == WidgetType::TYPES["Advertisement"]
 	end
