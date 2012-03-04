f871a54a609b211aba4ebbe629658c77ba935880
Ramakrishnan <raamkrish23@gmail.com>
2012-03-05 00:57:12 +0530

    Paginated AJAX resp for refinement

--- app/controllers/admin/refinement_controller.rb
+++ app/controllers/admin/refinement_controller.rb
@@ -1,8 +1,8 @@
 class Admin::RefinementController < ApplicationController
 layout 'admin'
   def show
-  	model = params[:type].camelize.constantize
-  	@refinements = model.paginate(:page => params[:page], :per_page => 10)
+  	model = params[:type].to_s.camelize.constantize
+  	@refinements = model.paginate(:page => params[:page], :per_page => 1)
   	render :layout => false	
   end
 end
