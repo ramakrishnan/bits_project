2ba9054e2d508346fd5050c9aa5b5e2fe80d43dc
Ramakrishnan <raamkrish23@gmail.com>
2012-03-17 22:29:19 +0530

    CSS clean up for grid and page layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def select_tag_for_categories(selected="")
+		select_tag "category", options_from_collection_for_select(Category.all, "id", "name")
+
+	end
+	
+	def select_tag_for_languages(selected="")
+		select_tag "language", options_from_collection_for_select(Language.all, "id", "name")
+	end	
 end
